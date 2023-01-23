6caf0eddc87248a7fdee780f6cb5384e3035d0b9
Alicia Kirkland <aliciamonet2694@gmail.com>
2023-01-22 19:44:50 -0600

    build a simple container

---
+++ cdk/typescript/simple-container/test/simple-container.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleContainer from '../lib/simple-container-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-container-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleContainer.SimpleContainerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
